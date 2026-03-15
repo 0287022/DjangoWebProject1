062d148664059547f73411010533d1dc5ea9d864
Phi Jones <0287022@schoolsnet.act.edu.au>
2026-03-16 10:28:24 +1100

    Created dynamic dropdown

--- DjangoWebProject1/MyApp1/views.py
+++ DjangoWebProject1/MyApp1/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import teacher
+from .models import courseArea
 from .forms import InputForm
 
 
 # Create your views here.
 def index(request):
     teach = teacher.objects.all()
-    
-    return render(request, "MyApp1/index.html", {'content' : teach})
+    areas = list(courseArea.objects.values_list('courseArea', flat=True).distinct())
+    print(areas)
+    return render(request, "MyApp1/index.html", {'content' : teach, 'areas': areas})
 
 def input_view(request):
 
