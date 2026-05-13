e935344bc52c69c6cabacd01ed1d8edb6ae4fa6b
Phi Jones <0287022@schoolsnet.act.edu.au>
2026-05-13 14:11:42 +1000

    Updated the model descriptions !!! NEEDS MORE THINGS TO BE USABLE !!!

--- DjangoWebProject1/MyApp1/models.py
+++ DjangoWebProject1/MyApp1/models.py
@@ -5,9 +5,24 @@ class teacher(models.Model):
     Area = models.CharField(max_length=30)
 
 class courseArea(models.Model):
-    courseArea = models.CharField(max_length=25)
+    courseArea = models.CharField(max_length=30)
     course = models.CharField(max_length=30)
+    courseAreaCode = models.CharField(max_length=4, default = "")
 
 class courseDescription(models.Model):
     courseNumber = models.IntegerField
     courseDesc = models.CharField(max_length=50)
+
+class frameworkStandards(models.Model):
+    courseAreaCode = models.CharField(max_length=4)
+    accreditation = models.CharField(max_length=1)
+    year = models.IntegerField()
+    frameworkStandards = models.TextField()
+
+class unitData(models.Model):
+    course = models.ForeignKey(courseArea, on_delete=models.CASCADE)
+    accreditation = models.CharField(max_length=1)
+    unitName = models.CharField(max_length=50)
+    unitValue = models.FloatField()
+    unitGoals = models.TextField()
+    unitDesc = models.TextField()
