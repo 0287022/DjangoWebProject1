8f9d960ae1562f68a3b894f69b72022a700f79ae
hyperbolia44031 <celestialofblob@gmail.com>
2026-05-21 21:14:48 +1000

    install

---
+++ DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\psjpsj4\Source\Repos\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
